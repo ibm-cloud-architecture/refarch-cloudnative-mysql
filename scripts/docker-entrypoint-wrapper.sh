cfc44059daa795166fd8126e5670849eac07c77b
CoreOS Admin <core@sgclient-docker-dal09.casenation.poc>
2016-09-21 02:00:01 +0000

    simplify by removing scripts for replication

--- scripts/docker-entrypoint-wrapper.sh
+++ scripts/docker-entrypoint-wrapper.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+my_hostname=`hostname`
+my_ip=`getent hosts ${my_hostname} | head -1 | awk '{print $1;}'`
 
 sed -i -e 's/server-id=.*/server-id='${SERVER_ID}'/' /etc/mysql/mysql.conf.d/binlog.cnf
 
+sed -i -e 's/report-host=.*/report-host='${my_ip}'/' /etc/mysql/mysql.conf.d/binlog.cnf
+    
 /usr/local/bin/docker-entrypoint.sh $@
+
+
+
