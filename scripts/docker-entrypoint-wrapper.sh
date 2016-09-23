add1eb2daa4c5ee7125d05e93b39fac699a6be42
CoreOS Admin <core@sgclient-docker-dal09.casenation.poc>
2016-09-23 00:09:09 +0000

    fix auto increment offset based on server id

--- scripts/docker-entrypoint-wrapper.sh
+++ scripts/docker-entrypoint-wrapper.sh
@@ -5,7 +5,7 @@ my_ip=`getent hosts ${my_hostname} | head -1 | awk '{print $1;}'`
 
 sed -i -e 's/server-id=.*/server-id='${SERVER_ID}'/' /etc/mysql/mysql.conf.d/binlog.cnf
 
-sed -i -e 's/auto-increment-offset = .*/auto-increment-offset = '$((SITE_MASK+1))'/' /etc/mysql/mysql.conf.d/auto_increment.cnf
+sed -i -e 's/auto-increment-offset = .*/auto-increment-offset = '$(((SERVER_ID % 2) + 1))'/' /etc/mysql/mysql.conf.d/auto_increment.cnf
 
 sed -i -e 's/report-host=.*/report-host='${my_ip}'/' /etc/mysql/mysql.conf.d/binlog.cnf
     
